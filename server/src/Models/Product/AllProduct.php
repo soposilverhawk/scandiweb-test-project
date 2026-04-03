6eaec1d03458642cf0a09b8e5265b754cef8c8f9
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2026-04-04 01:00:46 +0400

    create child all product model

---
+++ server/src/Models/Product/AllProduct.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models\Product;
+
+use App\Models\Category\Category;
+
+class AllProduct extends Product
+{
+    public function __construct(
+        int $id,
+        string $productUID,
+        string $name,
+        bool $inStock,
+        Category $categoryObject,
+        string $brand,
+        string $description,
+        array $gallery = [],
+        array $attributes = [],
+        array $prices = []
+    ) {
+        parent::__construct(
+            $id,
+            $productUID,
+            $name,
+            $inStock,
+            $categoryObject,
+            $brand,
+            $description,
+            $gallery,
+            $attributes,
+            $prices
+        );
+    }
+
+    public function getType(): string
+    {
+        return $this->categoryObject->getName();
+    }
+}
