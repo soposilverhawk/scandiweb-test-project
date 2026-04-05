7562b9ef0eeef6217659f0377d752b0a076fcb23
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2026-04-05 19:12:29 +0400

    Add Product GraphQL query

---
+++ server/src/GraphQL/Queries/ProductGraphQLQuery.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use GraphQL\Type\Definition\Type;
+use App\GraphQL\Types\ProductType;
+use App\GraphQL\Resolvers\ProductResolver;
+
+class ProductGraphQLQuery
+{
+    public static function getFields(ProductResolver $resolver): array
+    {
+        return [
+            'products' => [
+                'type' => Type::listOf(ProductType::getType()),
+                'resolve' => [$resolver, 'products']
+            ],
+            'product' => [
+                'type' => ProductType::getType(),
+                'args' => [
+                    'id' => Type::nonNull(Type::int())
+                ],
+                'resolve' => [$resolver, 'product']
+            ]
+        ];
+    }
+}
