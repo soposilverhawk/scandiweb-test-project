bcefbac995445ba4a9504e7319ddfcf176e8e140
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2026-04-05 19:42:59 +0400

    modify schema to use GraphQL Schema class in controller/GraphQL and resolvers to use factories

--- server/src/Controller/GraphQL.php
+++ server/src/Controller/GraphQL.php
@@ -2,61 +2,30 @@
 
 namespace App\Controller;
 
+use App\GraphQL\ResolverFactory\ResolverFactory;
 use GraphQL\GraphQL as GraphQLBase;
-use GraphQL\Type\Definition\ObjectType;
-use GraphQL\Type\Definition\Type;
-use GraphQL\Type\Schema;
-use GraphQL\Type\SchemaConfig;
+use App\GraphQL\Schema\GraphQLSchema;
 use RuntimeException;
 use Throwable;
 
-class GraphQL {
-    static public function handle() {
+class GraphQL
+{
+    static public function handle()
+    {
         try {
-            $queryType = new ObjectType([
-                'name' => 'Query',
-                'fields' => [
-                    'echo' => [
-                        'type' => Type::string(),
-                        'args' => [
-                            'message' => ['type' => Type::string()],
-                        ],
-                        'resolve' => static fn ($rootValue, array $args): string => $rootValue['prefix'] . $args['message'],
-                    ],
-                ],
-            ]);
-        
-            $mutationType = new ObjectType([
-                'name' => 'Mutation',
-                'fields' => [
-                    'sum' => [
-                        'type' => Type::int(),
-                        'args' => [
-                            'x' => ['type' => Type::int()],
-                            'y' => ['type' => Type::int()],
-                        ],
-                        'resolve' => static fn ($calc, array $args): int => $args['x'] + $args['y'],
-                    ],
-                ],
-            ]);
-        
-            // See docs on schema options:
-            // https://webonyx.github.io/graphql-php/schema-definition/#configuration-options
-            $schema = new Schema(
-                (new SchemaConfig())
-                ->setQuery($queryType)
-                ->setMutation($mutationType)
-            );
-        
+            $categoryResolver = ResolverFactory::makeCategoryResolver();
+            $productResolver = ResolverFactory::makeProductResolver();
+            $schema = GraphQLSchema::build($categoryResolver, $productResolver);
+
             $rawInput = file_get_contents('php://input');
             if ($rawInput === false) {
                 throw new RuntimeException('Failed to get php://input');
             }
-        
+
             $input = json_decode($rawInput, true);
             $query = $input['query'];
             $variableValues = $input['variables'] ?? null;
-        
+
             $rootValue = ['prefix' => 'You said: '];
             $result = GraphQLBase::executeQuery($schema, $query, $rootValue, null, $variableValues);
             $output = $result->toArray();
@@ -64,7 +33,7 @@ class GraphQL {
             $output = [
                 'error' => [
                     'message' => $e->getMessage(),
-                ],
+                ]
             ];
         }
 
