0db80a6e1489ee48ffbaf12b0345f9a8bfc5014b
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2026-04-05 16:35:10 +0400

    Add missing properties to product type and and nested data for product gallery, prices and attributes

--- server/src/GraphQL/Types/ProductType.php
+++ server/src/GraphQL/Types/ProductType.php
@@ -4,23 +4,49 @@ namespace App\GraphQL\Types;
 
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
+use App\GraphQL\Types\PriceType;
+use App\GraphQL\Types\AttributeType;
 
 class ProductType {
-    public static function getType(): ObjectType {
-        return new ObjectType([
+    private static ?ObjectType $instance = null;
+
+public static function getType(): ObjectType {
+    if (self::$instance === null) {
+        self::$instance = new ObjectType([
             'name' => 'Product',
-            'fields' => [
-                'id' => Type::nonNull(Type::int()),
-                'uid' => Type::nonNull(Type::string()),
-                'name' => Type::string(),
-                'stock' => Type::int(),
-                'category' => Type::string(),
-                'brand' => Type::string(),
-                'description' => Type::string(),
-                'gallery' => Type::listOf(Type::string()),
-                'prices' => Type::listOf(PriceType::getType()),
-                'attributes' => Type::listOf(AttributeType::getType()),
-            ],
+            'fields' => function () {
+                return [
+                    'id' => Type::nonNull(Type::int()),
+                    'product_uid' => Type::nonNull(Type::string()),
+                    'name' => Type::string(),
+                    'in_stock' => Type::boolean(),
+                    'category_name' => Type::string(),
+                    'category_id' => Type::int(),
+                    'brand' => Type::string(),
+                    'description' => Type::string(),
+                    'product_gallery' => [
+                        'type' => Type::listOf(Type::string()),
+                        'resolve' => function($product) {
+                            return $product['product_gallery'];
+                        }
+                    ],
+                    'product_prices' => [
+                        'type' => Type::listOf(PriceType::getType()),
+                        'resolve' => function($product) {
+                            return $product['product_prices'];
+                        }
+                    ],
+                    'product_attributes' => [
+                        'type' => Type::listOf(AttributeType::getType()),
+                        'resolve' => function($product) {
+                            return $product['product_attributes'];
+                        }
+                    ]
+                ];
+            }
         ]);
     }
+
+    return self::$instance;
+}
 }
