fbbea051da43e614cc6ebe01bd5466e0838ed9a3
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2026-04-09 15:33:46 +0400

    add children props to the content wrapper to render content nested in wrapper

--- client/src/components/ContentWrapper/ContentWrapper.jsx
+++ client/src/components/ContentWrapper/ContentWrapper.jsx
@@ -4,14 +4,13 @@ import styled from "styled-components";
 const Wrapper = styled.div`
     max-width: 1440px;
     margin: 0 auto;
-    background-color: red;
     padding: 2.4rem 0 1.5rem 0;
 `
 
-function ContentWrapper() {
+function ContentWrapper({children}) {
   return (
     <Wrapper>
-        test
+        {children}
     </Wrapper>
   )
 }
