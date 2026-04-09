4055978fbe39241f8f4c7a64406cd8d2b6cd7589
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2026-04-09 15:49:29 +0400

    move styled components definitions into their own file

--- client/src/components/ContentWrapper/ContentWrapper.jsx
+++ client/src/components/ContentWrapper/ContentWrapper.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-    max-width: 1440px;
-    margin: 0 auto;
-    padding: 2.4rem 0 1.5rem 0;
-`
+import { Wrapper } from './ContentWrapper.styles';
 
 function ContentWrapper({children}) {
   return (
---
+++ client/src/components/ContentWrapper/ContentWrapper.styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    max-width: 1440px;
+    margin: 0 auto;
+    padding: 2.4rem 0 1.5rem 0;
+`
