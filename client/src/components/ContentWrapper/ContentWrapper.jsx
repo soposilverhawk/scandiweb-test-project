378769fc5e5f7663237f72d341398769670222ee
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2026-04-09 14:40:56 +0400

    Add content wrapper component for consistent layout of the app

---
+++ client/src/components/ContentWrapper/ContentWrapper.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    max-width: 1440px;
+    margin: 0 auto;
+    background-color: red;
+    padding: 2.4rem 0 1.5rem 0;
+`
+
+function ContentWrapper() {
+  return (
+    <Wrapper>
+        test
+    </Wrapper>
+  )
+}
+
+export default ContentWrapper
