bab4bc04bcad4e973e16c9c421e3ab784e08b8e7
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2026-04-10 19:39:34 +0400

    setup apollo client for client side graphql operations

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:8000/graphql" }),
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </StrictMode>,
 );
