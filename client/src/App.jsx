355b9d7d96eaad93c0a31110f5a8dbd736b4bb27
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2026-04-10 20:59:17 +0400

    build out the application structure in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import ContentWrapper from './components/ContentWrapper/ContentWrapper';
+import Header from './components/Header/Header';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <ContentWrapper>
+      <Header />
+      <main>
+        <AppRoutes />
+      </main>
+    </ContentWrapper>
   )
 }
 
