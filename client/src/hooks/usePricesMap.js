cd8522031aae6df94845cf613549bf303c633880
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2026-04-21 19:23:52 +0400

    update usePricesMap hook to use the preffered currency context

--- client/src/hooks/usePricesMap.js
+++ client/src/hooks/usePricesMap.js
@@ -1,15 +1,18 @@
 // hooks/usePrice.js
 import { useMemo } from "react";
 import createPricesMap from "../utils/createPricesMap";
+import { usePrefferedCurrency } from "../context/PrefferedCurrencyContext";
 
-export function usePricesMap(productPrices, preferredCurrency = "$") {
+export function usePricesMap(productPrices) {
+  const { currency: preferredCurrency } = usePrefferedCurrency();
+
   return useMemo(() => {
     if (!productPrices?.length) {
       return {
         amount: 0,
         currency: preferredCurrency,
         pricesMap: {},
-      }
+      };
     }
 
     const pricesMap = createPricesMap(productPrices);
