2341c50ee5c856e2b94982ee9ebbe72f46edb4cd
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2026-04-24 18:08:17 +0400

    add height to the GalleryViewContainer styled component

--- client/src/pages/productDetailsPage/ProductDetailsPage.styles.js
+++ client/src/pages/productDetailsPage/ProductDetailsPage.styles.js
@@ -9,6 +9,7 @@ export const ProductPageSection = styled.section`
 export const GalleryViewContainer = styled.div`
   max-width: 60%;
   max-height: 47.8rem;
+  height: 47.8rem;
   width: 100%;
   gap: 15rem;
   display: flex;
