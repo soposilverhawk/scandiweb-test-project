427dcb988553d90eebaaec0111e9462cbbc8ecea
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2026-04-26 21:10:30 +0400

    adjust styling for AttributeList component

--- client/src/components/AttributeList/AttributeList.styles.js
+++ client/src/components/AttributeList/AttributeList.styles.js
@@ -3,7 +3,7 @@ import styled, { css } from "styled-components";
 export const StyledAttributesContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: ${({ $variant }) => ($variant === "pdp" ? "5rem" : "0.8rem")};
+  gap: ${({ $variant }) => ($variant === "pdp" ? "2rem" : "1.2rem")};
   font-size: ${({ $variant }) =>
     $variant === "pdp" ? "var(--product-details-text)" : "1.4rem"};
   font-weight: ${({ $variant }) => ($variant === "pdp" ? 700 : 300)};
@@ -24,7 +24,7 @@ export const AttributeSelectableButton = styled.button`
   font-size: ${({ $variant }) =>
     $variant === "pdp" ? "var(--text-standard-size)" : "1.4rem"};
   padding: ${({ $variant }) =>
-    $variant === "pdp" ? "1.3rem 2.3rem" : "0.2rem"};
+    $variant === "pdp" ? "1.3rem 2.3rem" : "0.2rem 0.5rem"};
   cursor: pointer;
 
   width: ${({ $attributeType, $variant }) =>
@@ -42,32 +42,42 @@ export const AttributeSelectableButton = styled.button`
       : "auto"};
 
   border: ${({ $attributeType, $active }) =>
-    $active
-      ? "2px solid black"
-      : $attributeType === "swatch"
-        ? "1px solid #fff"
+    $attributeType === "swatch"
+      ? $active
+        ? "2px solid var(--primary-color)"
+        : "2px solid #c4c4c4"
+      : $active
+        ? "1px solid black"
         : "1px solid var(--secondary-color)"};
 
   background-color: ${({ $attributeType, $attribute_item_value, $active }) =>
-    $active
-      ? "black"
-      : $attributeType === "swatch"
-        ? $attribute_item_value
+    $attributeType === "swatch"
+      ? $attribute_item_value
+      : $active
+        ? "black"
         : "transparent"};
 
-  color: ${({ $active }) => ($active ? "#fff" : "inherit")};
+  color: ${({ $attributeType, $active }) =>
+    $attributeType === "swatch" ? "transparent" : $active ? "#fff" : "inherit"};
 
-  ${({ $attributeType }) =>
-    $attributeType === "swatch"
-      ? css`
-          &:hover {
-            border: 1px solid var(--primary-color);
-          }
-        `
-      : css`
-          &:hover {
-            background-color: var(--secondary-color);
-            color: #fff;
-          }
-        `}
+  ${({ $attributeType, $variant }) =>
+    $variant === "pdp"
+      ? $attributeType === "swatch"
+        ? css`
+            &:hover {
+              border: 2px solid var(--primary-color);
+            }
+          `
+        : css`
+            &:hover {
+              background-color: var(--secondary-color);
+              color: #fff;
+            }
+          `
+      : ""}
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.8;
+  }
 `;
